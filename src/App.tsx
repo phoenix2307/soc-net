d343d8bbfb5b63b6c2df117c45d86774be43827c
Alex <phoenix.trade2307@gmail.com>
2022-02-13 15:25:09 +0200

    40-add-messages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile, {ProfilePropsType} from "./Components/Profile/Profile";
-import Dialogs, {DialogPagePropsType} from "./Components/Dialogs/Dialogs";
+import {DialogPagePropsType, Dialogs} from "./Components/Dialogs/Dialogs";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import News from "./Components/News/News";
 import Music from "./Components/Music/Music";
@@ -32,12 +32,16 @@ const App = (props: AppStatePropsType) => {
                             <Profile
                                 posts={props.state.profilePage.postsData}
                                 newPostText={props.state.profilePage.newPostText}
-                                dispatch={props.dispatch.bind(props.state)} // needed bind(props.state)????
+                                dispatch={props.dispatch.bind(props.state)} // needed bind(props.state)???
                             />}
                         />
                         <Route path='/dialogs/*' element={<Dialogs
                             dialogsData={props.state.dialogPage.dialogsData}
-                            messageData={props.state.dialogPage.messageData}/>}/>
+                            newMessageText={props.state.dialogPage.newMessageText}
+                            messageData={props.state.dialogPage.messageData}
+                            dispatch={props.dispatch.bind(props.state)}
+                        />}
+                        />
                         <Route path='/news/*' element={<News/>}/>
                         <Route path='/music/*' element={<Music/>}/>
                         <Route path='/settings/*' element={<Settings/>}/>
