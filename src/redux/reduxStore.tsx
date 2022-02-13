4797c721ae800865b0bd1388eac138695478c0a1
Alex <phoenix.trade2307@gmail.com>
2022-02-13 21:12:13 +0200

    42-redux

--- src/redux/reduxStore.tsx
+++ src/redux/reduxStore.tsx
@@ -3,11 +3,11 @@ import {profileReducer} from "./profileReducer";
 import {dialogsReducer} from "./dialogsReducer";
 import {sidebarReducer} from "./sidebarReducer";
 
-
 const reducers = combineReducers({
     profilePage: profileReducer,
     dialogPage: dialogsReducer,
     sidebar: sidebarReducer
 })
+
 
 export const store = createStore(reducers)
