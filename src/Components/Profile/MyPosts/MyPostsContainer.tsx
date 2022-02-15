3455ddb6c0853e85242ce452bc56d3f9f31ce7a2
Alex <phoenix.trade2307@gmail.com>
2022-02-15 13:12:54 +0200

    43-container component

---
+++ src/Components/Profile/MyPosts/MyPostsContainer.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import s from './MyPosts.module.css';
+import Post, {PostType} from "./Post/Post";
+import {addPostAC, GlobalActionType, updateNewPostAC} from "../../../redux/reduxStore";
+
+
+export type MyPostsType = {
+    posts: PostType[]
+    newPostText: string
+    dispatch: (action: GlobalActionType) => void
+}
+
+function MyPosts(props: MyPostsType) {
+
+    const postElements = props.posts.map(p => <Post
+        id={p.id}
+        message={p.message}
+        likesCount={p.likesCount}/>)
+
+    let newPostElement = React.createRef<HTMLTextAreaElement>()
+
+    const onClickHandler = () => {
+        let textPost = newPostElement.current?.value
+        if (textPost) {
+            props.dispatch(addPostAC(textPost))
+        }
+    }
+
+    const onChangeHandler = () => {
+        let newText =  newPostElement.current?.value
+        if(newText){
+            props.dispatch(updateNewPostAC(newText))
+        }
+    }
+
+    return (
+        <div className={s.postBlock}>
+            <h3>My posts</h3>
+            <div>
+                <textarea placeholder={'Write your thoughts here'}
+                          onChange={onChangeHandler}
+                          ref={newPostElement}
+                          value={props.newPostText}
+                >
+                </textarea>
+            </div>
+            <div>
+                <button onClick={onClickHandler}>Add post</button>
+            </div>
+            <div className={s.posts}>
+                {postElements}
+            </div>
+        </div>
+
+    )
+}
+
+export default MyPosts;
